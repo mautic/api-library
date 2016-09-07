1bb7c8555a5063a4576c91b1fe4f5c7dc1f70c16
Jan Linhart <admin@escope.cz>
2016-09-07 18:05:58 +0200

    Page write enpoints enabled, tests added

--- tests/Api/PagesTest.php
+++ tests/Api/PagesTest.php
@@ -28,4 +28,86 @@ class PagesTest extends MauticApiTestCase
         $message = isset($result['error']) ? $result['error']['message'] : '';
         $this->assertFalse(isset($result['error']), $message);
     }
+
+
+    public function testCreateAndDelete()
+    {
+        $pageApi = $this->getContext('pages');
+        $page    = $pageApi->create(
+            array(
+                'title' => 'test'
+            )
+        );
+
+        $message = isset($page['error']) ? $page['error']['message'] : '';
+        $this->assertFalse(isset($page['error']), $message);
+
+        //now delete the page
+        $result = $pageApi->delete($page['page']['id']);
+
+        $message = isset($result['error']) ? $result['error']['message'] : '';
+        $this->assertFalse(isset($result['error']), $message);
+    }
+
+    public function testEditPut()
+    {
+        $pageApi = $this->getContext('pages');
+        $page    = $pageApi->edit(
+            10000,
+            array(
+                'title' => 'test'
+            )
+        );
+
+        //there should be an error as the page shouldn't exist
+        $this->assertTrue(isset($page['error']), $page['error']['message']);
+
+        $page = $pageApi->create(
+            array(
+                'title' => 'test'
+            )
+        );
+
+        $message = isset($page['error']) ? $page['error']['message'] : '';
+        $this->assertFalse(isset($page['error']), $message);
+
+        $page = $pageApi->edit(
+            $page['page']['id'],
+            array(
+                'title' => 'test2'
+            )
+        );
+
+        $message = isset($page['error']) ? $page['error']['message'] : '';
+        $this->assertFalse(isset($page['error']), $message);
+
+        //now delete the page
+        $result = $pageApi->delete($page['page']['id']);
+
+        $message = isset($result['error']) ? $result['error']['message'] : '';
+        $this->assertFalse(isset($result['error']), $message);
+    }
+
+    public function testEditPatch()
+    {
+        $pageApi = $this->getContext('pages');
+        $page    = $pageApi->edit(
+            10000,
+            array(
+                'title' => 'test',
+                // following cannot be null
+                'isPublished' => 1
+            ),
+            true
+        );
+
+        $message = isset($page['error']) ? $page['error']['message'] : '';
+        $this->assertFalse(isset($page['error']), $message);
+
+        //now delete the page
+        $result = $pageApi->delete($page['page']['id']);
+
+        $message = isset($result['error']) ? $result['error']['message'] : '';
+        $this->assertFalse(isset($result['error']), $message);
+    }
 }
