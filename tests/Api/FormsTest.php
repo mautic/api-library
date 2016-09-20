bb79ce31539f481fb2c591bf39bf4d15682a3230
Jan Linhart <admin@escope.cz>
2016-09-20 12:37:25 +0200

    Creating/removing form fields and actions via PUT request tested

--- tests/Api/FormsTest.php
+++ tests/Api/FormsTest.php
@@ -11,6 +11,29 @@ namespace Mautic\Tests\Api;
 
 class FormsTest extends MauticApiTestCase
 {
+    protected $basicForm = array(
+        'name' => 'test',
+        'formType' => 'standalone',
+        'description' => 'API test',
+        'fields' => array(
+            array(
+                'label' => 'field name',
+                'type' => 'text'
+            )
+        ),
+        'actions' => array(
+            array(
+                'name' => 'action name',
+                'description' => 'action desc',
+                'type' => 'lead.pointschange',
+                'properties' => array(
+                    'operator' => 'plus',
+                    'points' => 2
+                )
+            )
+        )
+    );
+
     public function testGet()
     {
         $apiContext = $this->getContext('forms');
@@ -31,20 +54,21 @@ class FormsTest extends MauticApiTestCase
 
     public function testCreateAndDelete()
     {
-        $formApi = $this->getContext('forms');
-        $form    = $formApi->create(
-            array(
-                'name' => 'test',
-                'formType' => 'standalone',
-                'description' => 'API test'
-            )
-        );
+        $formApi  = $this->getContext('forms');
+        $response = $formApi->create($this->basicForm);
 
-        $message = isset($form['error']) ? $form['error']['message'] : '';
-        $this->assertFalse(isset($form['error']), $message);
+        $message = isset($response['error']) ? $response['error']['message'] : '';
+        $this->assertFalse(isset($response['error']), $message);
+
+        $this->assertFalse(empty($response['form']['fields']), 'The form field was not created.');
+        $this->assertFalse(empty($response['form']['actions']), 'The form action was not created.');
+        $lastField = array_pop($response['form']['fields']);
+        $lastAction = array_pop($response['form']['actions']);
+        $this->assertTrue($lastField['label'] === $this->basicForm['fields'][0]['label'], 'The form field name does not match');
+        $this->assertTrue($lastAction['name'] === $this->basicForm['actions'][0]['name'], 'The form action name does not match');
 
         //now delete the form
-        $result = $formApi->delete($form['form']['id']);
+        $result = $formApi->delete($response['form']['id']);
 
         $message = isset($result['error']) ? $result['error']['message'] : '';
         $this->assertFalse(isset($result['error']), $message);
@@ -53,45 +77,20 @@ class FormsTest extends MauticApiTestCase
     public function testEditPatch()
     {
         $formApi  = $this->getContext('forms');
-        $response = $formApi->edit(
-            10000,
-            array(
-                'name' => 'test',
-                'formType' => 'standalone',
-                'description' => 'API test'
-            )
-        );
+        $response = $formApi->edit(10000, $this->basicForm);
 
         //there should be an error as the form shouldn't exist
         $this->assertTrue(isset($response['error']), $response['error']['message']);
 
-        $response = $formApi->create(
-            array(
-                'name' => 'test',
-                'formType' => 'standalone',
-                'description' => 'API test',
-                'fields' => array(
-                    array(
-                        'label' => 'field name',
-                        'type' => 'text'
-                    )
-                ),
-                'actions' => array(
-                    array(
-                        'name' => 'action name',
-                        'description' => 'field desc',
-                        'type' => 'lead.pointschange',
-                        'properties' => array(
-                            'operator' => 'plus',
-                            'points' => 2
-                        )
-                    )
-                )
-            )
-        );
+        $response = $formApi->create($this->basicForm);
 
         $message = isset($response['error']) ? $response['error']['message'] : '';
         $this->assertFalse(isset($response['error']), $message);
+
+        $lastField = array_pop($response['form']['fields']);
+        $lastAction = array_pop($response['form']['actions']);
+        $lastField['label'] = 'edited field';
+        $lastAction['name'] = 'edited action';
 
         $response = $formApi->edit(
             $response['form']['id'],
@@ -99,28 +98,23 @@ class FormsTest extends MauticApiTestCase
                 'name' => 'test2',
                 'formType' => 'standalone',
                 'fields' => array(
-                    array(
-                        'id' => 73,
-                        'label' => 'field name',
-                        'type' => 'text'
-                    )
+                    $lastField
                 ),
                 'actions' => array(
-                    array(
-                        'name' => 'action name',
-                        'description' => 'field desc',
-                        'type' => 'lead.pointschange',
-                        'properties' => array(
-                            'operator' => 'plus',
-                            'points' => 2
-                        )
-                    )
+                    $lastAction
                 )
             )
         );
 
         $message = isset($response['error']) ? $response['error']['message'] : '';
         $this->assertFalse(isset($response['error']), $message);
+
+        $this->assertTrue(!empty($response['form']['fields']), 'The form field array is empty.');
+        $this->assertTrue(!empty($response['form']['actions']), 'The form action array is empty.');
+        $lastField = array_pop($response['form']['fields']);
+        $lastAction = array_pop($response['form']['actions']);
+        $this->assertTrue($lastField['label'] === 'edited field', 'The form field name does not match');
+        $this->assertTrue($lastAction['name'] === 'edited action', 'The form action name does not match');
 
         //now delete the form
         $result = $formApi->delete($response['form']['id']);
@@ -132,35 +126,42 @@ class FormsTest extends MauticApiTestCase
     public function testEditPut()
     {
         $formApi  = $this->getContext('forms');
+        $response = $formApi->edit(10000, $this->basicForm, true);
+
+        $message = isset($response['error']) ? $response['error']['message'] : '';
+        $this->assertFalse(isset($response['error']), $message);
+        $this->assertFalse(empty($response['form']['fields']), 'Fields were not added via PUT request');
+        $this->assertFalse(empty($response['form']['actions']), 'Actions were not added via PUT request');
+
+        //now delete the form
+        $result = $formApi->delete($response['form']['id']);
+
+        $message = isset($result['error']) ? $result['error']['message'] : '';
+        $this->assertFalse(isset($result['error']), $message);
+    }
+
+    public function testFieldAndActionDeleteViaPut()
+    {
+        $formApi  = $this->getContext('forms');
+
+        // Firstly create a form with fields
+        $response = $formApi->edit(10000, $this->basicForm, true);
+
+        // Remove fields and actions
+        unset($response['form']['fields']);
+        unset($response['form']['actions']);
+
+        // Edit the same entitiy without the fields and actions
         $response = $formApi->edit(
-            10000,
-            array(
-                'name' => 'test 2',
-                'description' => 'API test',
-                'formType' => 'standalone',
-                'fields' => array(
-                    array(
-                        'label' => 'field name 2',
-                        'type' => 'text'
-                    )
-                ),
-                'actions' => array(
-                    array(
-                        'name' => 'action name 2',
-                        'description' => 'field desc',
-                        'type' => 'lead.pointschange',
-                        'properties' => array(
-                            'operator' => 'plus',
-                            'points' => 2
-                        )
-                    )
-                )
-            ),
+            $response['form']['id'],
+            $response['form'],
             true
         );
 
         $message = isset($response['error']) ? $response['error']['message'] : '';
         $this->assertFalse(isset($response['error']), $message);
+        $this->assertTrue(empty($response['form']['fields']), 'Fields were not deleted via PUT request');
+        $this->assertTrue(empty($response['form']['actions']), 'Actions were not deleted via PUT request');
 
         //now delete the form
         $result = $formApi->delete($response['form']['id']);
