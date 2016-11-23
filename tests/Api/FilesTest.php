d5f8699416525e5a520fb06fae940d8c698914f0
Jan Linhart <admin@escope.cz>
2016-11-23 10:43:42 +0100

    Added test that check that API cannot upload a PHP file

--- tests/Api/FilesTest.php
+++ tests/Api/FilesTest.php
@@ -66,6 +66,18 @@ class FilesTest extends MauticApiTestCase
         $this->assertSuccess($response);
     }
 
+    public function testCreateAndDeletePhpScript()
+    {
+        $apiContext = $this->getContext('files');
+
+        // Get this PHP script to send
+        $this->testPayload['file'] = dirname(__DIR__).'/Api/FilesTest.php';
+        $this->assertTrue(file_exists($this->testPayload['file']), 'A file for test at '.$this->testPayload['file'].' does not exist.');
+
+        $response = $apiContext->create($this->testPayload);
+        $this->assertFalse(empty($response['error']), 'The PHP script was uploaded! Danger! DANGER!');
+    }
+
     public function testCreateAndDeleteImageInSubdir()
     {
         $apiContext = $this->getContext('files');
