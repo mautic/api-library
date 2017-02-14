00cc45c512738b26e7306c68969d5976bb8be1e2
Zdeno Kuzmany <zdeno@kuzmany.biz>
2017-02-14 15:19:29 +0100

    Init commit

---
+++ lib/Api/Focus.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @package     Mautic
+ * @copyright   2014 Mautic, NP. All rights reserved.
+ * @author      Mautic
+ * @link        http://mautic.org
+ * @license     MIT http://opensource.org/licenses/MIT
+ */
+
+namespace Mautic\Api;
+
+/**
+ * Emails Context
+ */
+class Focus extends Api
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $endpoint = 'focus';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $listName = 'focusitems';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $itemName = 'focusitem';
+
+
+    /**
+     * @param $id
+     * @return array|mixed
+     */
+    public function generateJs($id)
+    {
+        return $this->makeRequest($this->endpoint.'/'.$id.'/js', [] , 'POST');
+    }
+
+}
