5d2361af73f132bbaa4b87e3034ada266bf3c90f
John Linhart <admin@escope.cz>
2017-04-04 16:34:01 +0200

    Added Theme API endpoint support and tests

---
+++ lib/Api/Themes.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * @package     Mautic
+ * @copyright   2014 Mautic, NP. All rights reserved.
+ * @author      Mautic
+ * @link        http://mautic.org
+ * @license     MIT http://opensource.org/licenses/MIT
+ */
+
+namespace Mautic\Api;
+
+/**
+ * Themes Context
+ */
+class Themes extends Api
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $endpoint = 'themes';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $listName = 'themes';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $itemName = 'theme';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function edit($id, array $parameters, $createIfNotExists = false)
+    {
+        return $this->actionNotSupported('edit');
+    }
+
+    /**
+     * @param array $parameters
+     *
+     * @return array|mixed
+     */
+    public function create(array $parameters)
+    {
+        if (!isset($parameters['file'])) {
+            throw new \InvalidArgumentException('theme zip file must be set in parameters');
+        }
+
+        return parent::create($parameters);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function createBatch(array $parameters)
+    {
+        return $this->actionNotSupported('createBatch');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function editBatch(array $parameters, $createIfNotExists = false)
+    {
+        return $this->actionNotSupported('editBatch');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function deleteBatch(array $ids)
+    {
+        return $this->actionNotSupported('deleteBatch');
+    }
+}
