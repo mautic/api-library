75ac89f598830067d6f92bf5f8919576cf97974d
John Linhart <admin@escope.cz>
2017-06-07 11:08:53 +0200

    Webhook API endpoints added and tested

---
+++ lib/Api/Webhooks.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @package     Mautic
+ * @copyright   2014 Mautic, NP. All rights reserved.
+ * @author      Mautic
+ * @link        http://mautic.org
+ * @license     MIT http://opensource.org/licenses/MIT
+ */
+
+namespace Mautic\Api;
+
+/**
+ * Webhooks Context
+ */
+class Webhooks extends Api
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $endpoint = 'hooks';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $listName = 'hooks';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $itemName = 'hook';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $searchCommands = array(
+        'ids',
+        'is:published',
+        'is:unpublished',
+        'is:mine',
+        'is:uncategorized',
+        'category',
+    );
+}
