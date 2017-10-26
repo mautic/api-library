aec385080f6c2677a90fc9e18784966827c3a370
John Linhart <admin@escope.cz>
2017-10-26 14:19:03 +0200

    Tag endpoint and tests added

---
+++ lib/Api/Tags.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @package     Mautic
+ * @copyright   2014 Mautic, NP. All rights reserved.
+ * @author      Mautic
+ * @link        http://mautic.org
+ * @license     MIT http://opensource.org/licenses/MIT
+ */
+
+namespace Mautic\Api;
+
+/**
+ * Tags Context
+ */
+class Tags extends Api
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $endpoint = 'tags';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $listName = 'tags';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $itemName = 'tag';
+}
