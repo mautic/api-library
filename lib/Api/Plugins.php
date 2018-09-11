5b39c0cea723c8c9e3d9d254edde1939aac740d7
Zdeno Kuzmany <zdeno@kuzmany.biz>
2018-09-11 22:30:20 +0200

    New plugins endpoint

---
+++ lib/Api/Plugins.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @package     Mautic
+ * @copyright   2014 Mautic, NP. All rights reserved.
+ * @author      Mautic
+ * @link        http://mautic.org
+ * @license     MIT http://opensource.org/licenses/MIT
+ */
+
+namespace Mautic\Api;
+
+/**
+ * Plugins Context
+ */
+class Plugins extends Api
+{
+
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $endpoint = 'plugins';
+
+
+    /**
+     * @param $integrationName
+     *
+     * @return array|mixed
+     */
+    public function getSettings($integrationName)
+    {
+        return $this->makeRequest($this->endpoint.'/settings/'.$integrationName);
+    }
+
+}
