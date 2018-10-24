f441637350b847b5cbc34a6ddb8dbfbc4be488fa
John Linhart <admin@escope.cz>
2018-10-24 14:58:46 +0200

    Support and tests for report API endpoint pagination added

--- lib/Api/Reports.php
+++ lib/Api/Reports.php
@@ -47,4 +47,33 @@ class Reports extends Api
         'get',
         'getList'
     );
+
+    /**
+     * Get a single report data
+     *
+     * @param int $id
+     * @param int $limit
+     * @param int $page
+     * @param \DateTimeInterface|null $dateFrom
+     * @param \DateTimeInterface|null $dateTo
+     *
+     * @return array|mixed
+     */
+    public function get($id, $limit = 10, $page = 1, \DateTimeInterface $dateFrom = null, \DateTimeInterface $dateTo = null)
+    {
+        $options = array(
+            'limit' => (int) $limit,
+            'page'  => (int) $page,
+        );
+
+        if ($dateFrom) {
+            $options['dateFrom'] = $dateFrom->format('c');
+        }
+
+        if ($dateTo) {
+            $options['dateTo'] = $dateTo->format('c');
+        }
+
+        return $this->makeRequest("{$this->endpoint}/$id", $options);
+    }
 }
--- tests/Api/ReportsTest.php
+++ tests/Api/ReportsTest.php
@@ -11,14 +11,42 @@ namespace Mautic\Tests\Api;
 
 class ReportsTest extends MauticApiTestCase
 {
-    public function setUp() {
+    public function setUp()
+    {
         $this->api = $this->getContext('reports');
     }
 
     public function testGet()
     {
         $response = $this->api->get(1);
+
         $this->assertErrors($response);
+        $this->assertGreaterThanOrEqual(0, $response['totalResults']);
+        $this->assertGreaterThanOrEqual(0, count($response['totalResults']));
+        $this->assertGreaterThanOrEqual(0, count($response['dataColumns']));
+        $this->assertSame(10, $response['limit']);
+        $this->assertSame(1, $response['page']);
+        $this->assertSame(25, strlen($response['dateFrom']));
+        $this->assertSame(25, strlen($response['dateTo']));
+    }
+
+    public function testGetCustom()
+    {
+        $limit    = 5;
+        $page     = 2;
+        $dateFrom = new \DateTimeImmutable('1 year ago', new \DateTimeZone('UTC'));
+        $dateTo   = new \DateTimeImmutable('now', new \DateTimeZone('UTC'));
+        $response = $this->api->get(1, $limit, $page, $dateFrom, $dateTo);
+
+        $this->assertErrors($response);
+        $this->assertGreaterThanOrEqual(0, $response['totalResults']);
+        $this->assertGreaterThanOrEqual(0, count($response['totalResults']));
+        $this->assertGreaterThanOrEqual(0, count($response['dataColumns']));
+        $this->assertSame($limit, $response['limit']);
+        $this->assertSame($page, $response['page']);
+        // Mautic will modify the times slightly so check only for date.
+        $this->assertSame($dateFrom->format('Y-m-d'), (new \DateTimeImmutable($response['dateFrom']))->format('Y-m-d'), 'DateFrom does not match');
+        $this->assertSame($dateTo->format('Y-m-d'), (new \DateTimeImmutable($response['dateTo']))->format('Y-m-d'), 'DateTo does not match');
     }
 
     public function testGetList()
