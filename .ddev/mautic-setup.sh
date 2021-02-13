ce9b33a6b908db6a0b4792771b17bd74743435e7
Dennis Ameling <dennis@dennisameling.com>
2021-02-13 22:28:54 +0100

    Enable Twilio plugin in DDEV setup

--- .ddev/mautic-setup.sh
+++ .ddev/mautic-setup.sh
@@ -19,6 +19,10 @@ setup_mautic() {
         --mailer_transport="smtp" --mailer_host="localhost" --mailer_port="1025"
     php bin/console cache:warmup --no-interaction --env=dev
 
+    printf "Enabling Twilio plugin for tests...\n"
+    mysql -udb -pdb -P3306 -hdb -e "USE db; INSERT INTO plugin_integration_settings (id, plugin_id, name, is_published, supported_features, api_keys, feature_settings) VALUES (2, NULL, 'Twilio', 1, 'a:0:{}', 'a:2:{s:8:\"username\";s:169:\"bzFmNlIydWRSZXlIN2lQVkdpanJ4aTQ2NUh6RVdDbHlLRVhsWGZ4b0kyZVNxLzYrQ1J6V1RvMnlhVEp0c245TEp6eStQekx5ZVhLWjB1YVdoR3RnR2dHQ3k1emVVdGt5NzZKUmtjUnJ3c1E9|L8tbZRIYhwatT7Mq+HAdYA==\";s:8:\"password\";s:169:\"T2d2cFpXQWE5YVZnNFFianJSYURRYUtGRHBNZGZjM1VETXg2Wm5Va3NheW43MjVWUlJhTVlCL2pYMDBpbElONStiVVBNbEM3M3BaeGJMNkFKNUFEN1pTNldSRjc4bUM4SDh1SE9OY1k5MTg9|TeuSvfx4XSUOvp0O7T49Cg==\";}', 'a:4:{s:20:\"sending_phone_number\";N;s:22:\"disable_trackable_urls\";i:0;s:16:\"frequency_number\";N;s:14:\"frequency_time\";N;}');"
+    php bin/console mautic:plugins:reload
+
     tput setaf 2
     printf "All done! Run \"ddev exec composer test\" to run PHPUnit tests.\n"
     tput sgr0
