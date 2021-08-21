6e03aecfa15a29699b491c62bf2e417ef03570e4
Zdeno Kuzmany <zdeno@kuzmany.biz>
2021-08-22 01:56:23 +0200

    Finish Client Credentials grant type, add to readme and remove oauth1 from readme

--- lib/Auth/TwoLeggedOAuth2.php
+++ lib/Auth/TwoLeggedOAuth2.php
@@ -9,48 +9,6 @@
  * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
 
-/*
-|--------------------------------------------------------------------------
-| 2-legged OAuth2 using the grant_type  client_credentials
-|--------------------------------------------------------------------------
-|
-| use Mautic\Auth\ApiAuth;
-|
-| // ApiAuth->newAuth() will accept an array of Auth settings
-| $settings = array(
-|     'AuthMethod'       => '2LeggedOAuth2' // Must be one of 'OAuth' or 'BasicAuth'
-      'clientKey'        => '',       // Client/Consumer key from Mautic
-      'clientSecret'     => '',       // Client/Consumer secret key from Mautic
-|     'baseUrl'           => '',         // NOTE: Required for Unit tests; *must* contain a valid url
-| );
-|
-| // Initiate the auth object
-| $initAuth = new ApiAuth();
-| $auth = $initAuth->newAuth($settings, $settings['AuthMethod']);
-|
-|--------------------------------------------------------------------------
-| Basic API Usage
-|--------------------------------------------------------------------------
-|
-| To use, just pass the auth object to the Api context you are creating.
-|
-| use Mautic\MauticApi;
-|
-| // Get a Contact context
-| $api = new MauticApi();
-| $contactApi = $api->newApi('contacts', $auth, $settings['apiUrl']);
-|
-| // Get Contact list
-| $results = $contactApi->getList();
-|
-| Note: If the credentials are incorrect an error response will be returned:
-| array('errors' => [[
-|       'code'    => 403,
-|       'message' => 'access_denied: OAuth2 authentication required' )
-| ]]
-|
-*/
-
 namespace Mautic\Auth;
 
 use Mautic\Exception\RequiredParameterMissingException;
@@ -103,7 +61,7 @@ class TwoLeggedOAuth2 extends AbstractAuth
      *
      * @throws RequiredParameterMissingException
      */
-    public function setup($baseUrl, $clientKey, $clientSecret)
+    public function setup($baseUrl, $clientKey, $clientSecret, $accessToken = null)
     {
         // we MUST have the username and password. No Blanks allowed!
         //
@@ -119,9 +77,10 @@ class TwoLeggedOAuth2 extends AbstractAuth
             );
         }
 
-        $this->baseurl      = $baseUrl;
-        $this->clientKey    = $clientKey;
-        $this->clientSecret = $clientSecret;
+        $this->baseurl       = $baseUrl;
+        $this->clientKey     = $clientKey;
+        $this->clientSecret  = $clientSecret;
+        $this->_access_token = $accessToken;
 
         if (!$this->_access_token_url) {
             $this->_access_token_url = $baseUrl.'/oauth/v2/token';
@@ -136,8 +95,9 @@ class TwoLeggedOAuth2 extends AbstractAuth
      */
     protected function prepareRequest($url, array $headers, array $parameters, $method, array $settings)
     {
-        //Set Basic Auth parameters/headers
-      //  $headers = array_merge($headers, ['Authorization: Bearer '.$this->_access_token]);
+        if ($this->_access_token !== null) {
+            $headers = array_merge($headers, ['Authorization: Bearer '.$this->_access_token]);
+        }
 
         return [$headers, $parameters];
     }
@@ -147,14 +107,15 @@ class TwoLeggedOAuth2 extends AbstractAuth
      */
     public function getAccessToken(): string
     {
-
-        $parameters = [
+        $parameters      = [
             'client_id'     => $this->clientKey,
             'client_secret' => $this->clientSecret,
             'grant_type'    => 'client_credentials',
         ];
-        $params = $this->makeRequest($this->_access_token_url, $parameters, 'POST');
-        print_r($params);
-        die();
+        $accessTokenData = $this->makeRequest($this->_access_token_url, $parameters, 'POST');
+        //store access token data however you want
+        $this->_access_token = $accessTokenData['access_token'] ?? null;
+
+        return $this->_access_token;
     }
 }
