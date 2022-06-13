6ce2765ae3bd271bf71b7cc6f5a0efc42d046a7a
Mattias Michaux <mattias.michaux@dropsolid.com>
2022-06-13 22:49:04 +0200

    simplify the used password algorithm

---
+++ .github/ci-files/security_local.php
@@ -0,0 +1,21 @@
+<?php
+
+$loader->import('security.php');
+
+$container->loadFromExtension(
+    'security',
+    [
+        'encoders' => [
+            'Symfony\Component\Security\Core\User\User' => [
+                'algorithm'  => 'md5',
+                'encode_as_base64' => false,
+                'iterations' => 0
+            ],
+            'Mautic\UserBundle\Entity\User' => [
+                'algorithm'  => 'md5',
+                'encode_as_base64' => false,
+                'iterations' => 0
+            ],
+        ],
+    ]
+);
