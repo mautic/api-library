53d29ce731b2fe839a62a5080c2223bac1f1fa3f
Mattias Michaux <mattias.michaux@dropsolid.com>
2022-06-13 23:21:54 +0200

    let's try this

--- .github/ci-files/security_local.php
+++ .github/ci-files/security_local.php
@@ -8,13 +8,13 @@ $container->loadFromExtension(
         'encoders' => [
             'Symfony\Component\Security\Core\User\User' => [
                 'algorithm'  => 'md5',
-                'encode_as_base64' => false,
-                'iterations' => 0
+  //              'encode_as_base64' => false,
+  //              'iterations' => 0
             ],
             'Mautic\UserBundle\Entity\User' => [
                 'algorithm'  => 'md5',
-                'encode_as_base64' => false,
-                'iterations' => 0
+  //              'encode_as_base64' => false,
+  //              'iterations' => 0
             ],
         ],
     ]
