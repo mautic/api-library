585ad804d20af1c9bbf0a298219cf29723545cf6
Mattias Michaux <mattias.michaux@dropsolid.com>
2023-02-10 22:27:14 +0100

    faster tests? + logs

--- .github/ci-files/prepare_mautic.sh
+++ .github/ci-files/prepare_mautic.sh
@@ -5,16 +5,46 @@ set -e
 cd /var/www/html
 php bin/console mautic:install http://localhost/ --force --mailer_from_name="GitHub Actions" --mailer_from_email="github-actions@mautic.org" --mailer_transport="smtp" --mailer_host="mailhog" --mailer_port="1025" --admin_username=admin --admin_password=mautic --admin_email="bla@bla.be"
 
-cat << EOF > app/config/parameters_local.php
+cat << 'EOF' > app/config/parameters_local.php
 <?php
 /**
  * Parameter overrides for GitHub Actions.
  */
-\$parameters = [
+$parameters = [
     'api_enabled'           => true,
     'api_enable_basic_auth' => true,
 ];
 EOF
+
+cat << 'EOF' > app/config/security_local.php
+<?php
+
+$this->import('security.php');
+
+// Support HTTP basic auth for test logins
+$container->loadFromExtension('security',
+    [
+        'firewalls' => [
+            'main' => [
+                // Support HTTP basic auth for test logins
+                'http_basic' => true,
+            ],
+        ],
+        'encoders'  => [
+          'Symfony\Component\Security\Core\User\User' => [
+            'algorithm'        => 'md5',
+            'encode_as_base64' => false,
+            'iterations'       => 0,
+          ],
+          'Mautic\UserBundle\Entity\User' => [
+            'algorithm'        => 'md5',
+            'encode_as_base64' => false,
+            'iterations'       => 0,
+          ],
+        ],
+    ]
+);
+
 
 php bin/console cache:clear --no-interaction
 
