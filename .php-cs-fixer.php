a40df184c80ecdb72eded6b57b0b90a57ff7995d
Jan Kozak <galvani78@gmail.com>
2023-12-01 11:19:10 +0100

    reset to mautic phpcs fixer

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -2,30 +2,23 @@
 
 $finder = PhpCsFixer\Finder::create()
     ->in(__DIR__.'/lib')
-    ;
+    ->in(__DIR__.'/tests');
 
-return (new PhpCsFixer\Config())
+$config = new PhpCsFixer\Config();
+
+return $config
     ->setRules([
-        '@Symfony'               => true,
+        '@Symfony' => true,
         'binary_operator_spaces' => [
             'operators' => [
+                '=' => 'align',
                 '=>' => 'align',
-                '='  => 'align',
             ],
         ],
-        'phpdoc_to_comment' => false,
-        'ordered_imports'   => true,
-        'array_syntax'      => [
-            'syntax' => 'short',
+        'ordered_imports' => true,
+        'array_syntax' => [
+            'syntax' => 'short'
         ],
-        'no_unused_imports' => true,
-        /**
-         * Our templates rely heavily on things like endforeach, endif, etc.
-         * This setting should be turned off at least until we've switched to Twig
-         * (which is required for Symfony 5).
-         */
-        'no_alternative_syntax' => false,
-        'phpdoc_summary'        => false,
-        'yoda_style'            => false,
+        'no_unused_imports' => false,
     ])
     ->setFinder($finder);
